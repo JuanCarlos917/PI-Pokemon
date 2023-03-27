67967472defa90b2e257f9f16831834c1d2c320c
Juan Gomez <juancarlos.gm931@gmail.com>
2023-03-27 17:27:43 -0500

    Se instala en formato dev Nodemon. Se agrega ,{timestamps: false} a Type

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+// Luego le inyectamos la conexion a sequelize.
 module.exports = (sequelize) => {
 	// defino el modelo
 	sequelize.define('pokemon', {
@@ -13,7 +13,7 @@ module.exports = (sequelize) => {
 			type: DataTypes.STRING,
 			allowNull: false,
 		},
-		image: {
+		imagen: {
 			type: DataTypes.STRING,
 			allowNull: false,
 		},
--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -1,17 +1,21 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+// Luego le inyectamos la conexion a sequelize.
 module.exports = (sequelize) => {
 	// defino el modelo
-	sequelize.define('type', {
-		id: {
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			allowNull: false,
+	sequelize.define(
+		'type',
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				allowNull: false,
+			},
+			nombre: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
 		},
-        nombre: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        }
-	});
+		{ timestamps: false },
+	);
 };
