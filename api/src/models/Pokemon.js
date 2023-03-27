d3582f038a0ed61bea44e47fc770cc97e6114864
Juan Gomez <juancarlos.gm931@gmail.com>
2023-03-27 17:11:47 -0500

    Se agregan las propiedades a los modelos Pokemon y Type

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -2,11 +2,44 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+	// defino el modelo
+	sequelize.define('pokemon', {
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			allowNull: false,
+		},
+		nombre: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		vida: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		ataque: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		defensa: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		velocidad: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		altura: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		peso: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	});
 };
