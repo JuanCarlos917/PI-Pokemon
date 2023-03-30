ab88402fa76e3be15d8d39d1030cfe98e19414f0
Juan Gomez <juancarlos.gm931@gmail.com>
2023-03-30 17:43:14 -0500

    Se crean carpetas en componentes

---
+++ client/src/redux/actions/index.js
@@ -0,0 +1,103 @@
+import {
+	GET_ALL_POKEMONS,
+	GET_ALL_TYPES,
+	GET_BY_ID,
+	CLEAR,
+	SEARCH_POKEMON,
+	FILTER_ORDER,
+	PAGINATION,
+	PAGES,
+	NEW_POKEMON,
+	CHANGE_PAGE,
+} from './actions-types';
+import axios from 'axios';
+
+const BACKEND = process.env.REACT_APP_BACK;
+
+export function getAllPokemons() {
+    return async function (dispatch) {
+        const response = await axios.get(`${BACKEND}/pokemons`);
+        return dispatch({
+            type: GET_ALL_POKEMONS,
+            payload: response.data,
+        });
+    };
+}
+
+export function getAllTypes() {
+    return async function (dispatch) {
+        const response = await axios.get(`${BACKEND}/types`);
+        return dispatch({
+            type: GET_ALL_TYPES,
+            payload: response.data,
+        });
+    };
+}
+
+export function getById(id) {
+    return async function (dispatch) {
+        const response = await axios.get(`${BACKEND}/pokemons/${id}`);
+        return dispatch({
+            type: GET_BY_ID,
+            payload: response.data,
+        });
+    };
+}
+
+export function clear() {
+    return {
+        type: CLEAR,
+    };
+}
+
+export function searchPokemon(name) {
+    return {
+        type: SEARCH_POKEMON,
+        payload: name,
+    };
+}
+
+export function filterOrder(order) {
+    return {
+        type: FILTER_ORDER,
+        payload: order,
+    };
+}
+
+export function pagination(page) {
+    return {
+        type: PAGINATION,
+        payload: page,
+    };
+}
+
+export function pages(page) {
+    return {
+        type: PAGES,
+        payload: page,
+    };
+}
+
+export function newPokemon(payload) {
+    return async function (dispatch) {
+        const response = await axios.post(`${BACKEND}/pokemons`, payload);
+        return dispatch({
+            type: NEW_POKEMON,
+            payload: response.data,
+        });
+    };
+}
+
+export function changePage(page) {
+    return {
+        type: CHANGE_PAGE,
+        payload: page,
+    };
+}
+
+
+
+
+
+
+
---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,83 @@
+import {
+	GET_ALL_POKEMONS,
+	GET_ALL_TYPES,
+	GET_BY_ID,
+	CLEAR,
+	SEARCH_POKEMON,
+	FILTER_ORDER,
+	PAGINATION,
+	PAGES,
+	NEW_POKEMON,
+	CHANGE_PAGE,
+} from '../actions/actions-types';
+
+const initialState = {
+    pokemons: [],
+    types: [],
+    pokemonDetail: {},
+    pokemonCreated: {},
+    search: '',
+    order: '',
+    pagination: 9,
+    pages: 0,
+    page: 1,
+};
+
+function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        case GET_ALL_POKEMONS:
+            return {
+                ...state,
+                pokemons: action.payload,
+            };
+        case GET_ALL_TYPES:
+            return {
+                ...state,
+                types: action.payload,
+            };
+        case GET_BY_ID:
+            return {
+                ...state,
+                pokemonDetail: action.payload,
+            };
+        case CLEAR:
+            return {
+                ...state,
+                pokemonDetail: {},
+            };
+        case SEARCH_POKEMON:
+            return {
+                ...state,
+                search: action.payload,
+            };
+        case FILTER_ORDER:
+            return {
+                ...state,
+                order: action.payload,
+            };
+        case PAGINATION:
+            return {
+                ...state,
+                pagination: action.payload,
+            };
+        case PAGES:
+            return {
+                ...state,
+                pages: action.payload,
+            };
+        case NEW_POKEMON:
+            return {
+                ...state,
+                pokemonCreated: action.payload,
+            };
+        case CHANGE_PAGE:
+            return {
+                ...state,
+                page: action.payload,
+            };
+        default:
+            return state;
+    }
+}
+
+export default rootReducer;
