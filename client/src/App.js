b2ae454afda382de5edf82e9c70b01d227868484
Juan Gomez <juancarlos.gm931@gmail.com>
2023-03-31 15:16:32 -0500

    Se crean componentes necesarios y se configura Store en index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
+import Home from './components/Home/Home';
+import DetailPokemon from './components/DetailPokemon/DetailPokemon';
+import FormNewPokemon from './components/FormNewPokemon/FormNewPokemon';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+		<div className='App'>
+			<LandingPage />
+			<Home />
+			<DetailPokemon />
+            <FormNewPokemon/>
+		</div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<Provider store={store}>
+		<React.StrictMode>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</React.StrictMode>
+		,
+	</Provider>,
 );
 
+//cambia el nombre de la pestaña
+let previousTitle = document.title;
+window.addEventListener('blur', () => {
+	previousTitle = document.title;
+	document.title = 'No te vayas!! Vuelve';
+});
+
+//devuelve el titulo original
+window.addEventListener('focus', () => {
+	document.title = previousTitle;
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
