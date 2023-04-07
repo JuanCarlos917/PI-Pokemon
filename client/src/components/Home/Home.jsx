06c6281ed3be1dee3422c87a46f8239f8b10233f
Juan Gomez <juancarlos.gm931@gmail.com>
2023-04-07 14:37:41 -0500

    Se renderiza componete Home con lo que indica la consigna

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,67 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {useDispatch, useSelector} from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAllPokemons } from '../../redux/actions';
-import { useEffect, useState } from 'react';
+import Pagination from '../Pagination/Pagination';
+import SearchBar from '../SearchBar/SearchBar';
+import './Home.module.css';
 
 export default function Home() {
-    const dispatch = useDispatch();
-    const pokemons = useSelector((state) => state.pokemons);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [pokemonsPerPage, setPokemonsPerPage] = useState(9);
-    const indexOfLastPokemon = currentPage * pokemonsPerPage;
-    const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage;
-    const currentPokemons = pokemons.slice(indexOfFirstPokemon, indexOfLastPokemon);
+	const dispatch = useDispatch();
+	const { pokemons } = useSelector((state) => state);
 
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(pokemons.length / pokemonsPerPage); i++) {
-        pageNumbers.push(i);
-    }
 
-    useEffect(() => {
-        dispatch(getAllPokemons());
-    }, [dispatch]);
+	useEffect(() => {
+		dispatch(getAllPokemons());
+	}, [dispatch]);
 
-    return (
-        <div className="home">
-            <div className="home__container">
-            <h1>Hola</h1>
-                <div className="home__container__cards">
-                    {currentPokemons.map((pokemon) => (
-                        <div className="home__container__cards__card" key={pokemon.id}>
-                            <Link to={`/pokemons/${pokemon.id}`}>
-                                <img
-                                    className="home__container__cards__card__img"
-                                    src={pokemon.img}
-                                    alt={pokemon.name}
-                                />
-                                <h3 className="home__container__cards__card__name">
-                                    {pokemon.name}
-                                </h3>
-                            </Link>
-                        </div>
-                    ))}
-                </div>
-                <div className="home__container__pagination">
-                    <ul className="home__container__pagination__list">
-                        {pageNumbers.map((number) => (
-                            <li
-                                className="home__container__pagination__list__item"
-                                key={number}
-                            >
-                                <button
-                                    className="home__container__pagination__list__item__button"
-                                    onClick={() => setCurrentPage(number)}
-                                >
-                                    {number}
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<SearchBar />
+			<h1>Pokemons</h1>
+			<div>
+				{pokemons.map((pokemon) => (
+					<div key={pokemon.id}>
+						<h3>{pokemon.name}</h3>
+						<img src={pokemon.image} alt={pokemon.name} />
+						<h4>Type:{pokemon.type1}</h4>
+					</div>
+				))}
+			</div>
+			<div>
+				<Pagination />
+			</div>
+		</div>
+	);
 }
-
-
