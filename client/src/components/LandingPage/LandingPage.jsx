63a3b9aaac95b77974fda6d4da163aa36e1a5b93
Juan Gomez <juancarlos.gm931@gmail.com>
2023-04-13 09:20:28 -0500

    Se agrega imagen a la landing Page

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './LandingPage.module.css';
+import imageLandingPage from '../../assets/LandingPage.jpg'
 
 export default function LandingPage() {
 
@@ -9,13 +10,14 @@ export default function LandingPage() {
 			<h1 className={styles.titleLandingPage}>PIKAPI</h1>
 			<div>
 				<Link to='/home'>
-					<button
-						className={styles.buttonLandingPage}
-						type='button'>
+					<button className={styles.buttonLandingPage} type='button'>
 						HOME
 					</button>
 				</Link>
 			</div>
+            <div className={styles.imageLandingPage}>
+			<img className={styles.image} src={imageLandingPage} alt='' />
+            </div>
 		</div>
 	);
 }
